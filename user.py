2acd0846661e74db334222377edd740622611355
make in bvb <70434165+Sadiq8064@users.noreply.github.com>
2025-11-19 14:49:02 +0530

    Update user.py

--- user.py
+++ user.py
@@ -94,7 +94,7 @@ async def _send_brevo_email(to_email: str, subject: str, html_content: str):
     }
 
     payload = {
-        "sender": {"name": "Smart Local Helpdesk", "email": "mrsadiq471@gmail.com"},
+        "sender": {"name": "Smart Local Helpdesk", "email": "no-reply@brevo.com"},
         "to": [{"email": to_email}],
         "subject": subject,
         "htmlContent": html_content
